8a42d9b7488c489aac92a7bf53e1df7d180a25a8
lizunlong <lizunlong@gmail.com>
2019-09-28 20:33:06 +0800

    v0.3.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,10 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+
+  eleventyConfig.addNunjucksFilter("date", function(value) {
+    const year = value.getFullYear();
+    const month = value.getMonth() + 1;
+    const day = value.getDate();
+    return `${year}年${month}月${day}日`;
+  });
 };
