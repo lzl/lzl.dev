5c7c541116513f6f1b60f2a6d7506c10d8674732
lizunlong <lizunlong@gmail.com>
2020-09-06 16:49:47 +0800

    show page title at head

--- layouts/index.js
+++ layouts/index.js
@@ -1,5 +1,14 @@
+import Head from 'next/head'
 import LeftRight from '@components/LeftRight'
 
-export default function Index({ children }) {
-  return <LeftRight right={children} />
+export default function Index({ children, frontMatter }) {
+  const { title } = frontMatter
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <LeftRight right={children} />
+    </>
+  )
 }
