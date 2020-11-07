52586c8ad644b504aad2e3a1a7048ddec6a24eb4
lizunlong <lizunlong@gmail.com>
2020-11-07 23:45:32 +0800

    auth with fauna

---
+++ src/pages/profile.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
+
+import LeftRight from '@/components/LeftRight'
+import { withAuthSync } from '@/utils/auth'
+
+function Left() {
+  return <p>Next</p>
+}
+
+function Right() {
+  const router = useRouter()
+  const { isLoading, error, data } = useQuery('profile', () =>
+    fetch('/api/profile').then((res) => {
+      if (res.status >= 300) {
+        throw new Error('API Client error')
+      }
+
+      return res.json()
+    })
+  )
+
+  useEffect(() => {
+    if (error) router.push('/')
+  }, [error, router])
+
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>An error has occurred: {error.message}</p>
+  return <p>{data.userId}</p>
+}
+
+export default withAuthSync(function Profile() {
+  return <LeftRight left={<Left />} right={<Right />} />
+})
