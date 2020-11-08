b9e7eb3c4785d95f26c3462fb0d16a925882aa9b
lizunlong <lizunlong@gmail.com>
2020-11-08 15:28:43 +0800

    add links

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,11 +3,31 @@ import Link from 'next/link'
 const data = [
   {
     title: 'Google',
-    url: 'https://google.com/',
+    url: 'https://google.com/ncr',
   },
   {
     title: 'Gmail',
-    url: 'https://mail.google.com/',
+    url: 'https://google.com/gmail',
+  },
+  {
+    title: 'Roam Research',
+    url: 'https://roamresearch.com/',
+  },
+  {
+    title: 'Standard Notes',
+    url: 'https://standardnotes.org/',
+  },
+  {
+    title: 'GitHub',
+    url: 'https://github.com/',
+  },
+  {
+    title: 'Vercel',
+    url: 'https://vercel.com/',
+  },
+  {
+    title: 'Fauna',
+    url: 'https://fauna.com/',
   },
 ]
 
