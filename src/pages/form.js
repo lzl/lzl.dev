56268ac1ea16f55264f992aad9f6cc99ec0277e8
lizunlong <lizunlong@gmail.com>
2020-11-22 21:42:44 +0800

    apply ring to the button

--- src/pages/form.js
+++ src/pages/form.js
@@ -107,7 +107,7 @@ function Form() {
           </div>
         </div>
         <div className="px-4 py-3 text-right bg-gray-50 sm:px-6">
-          <button className="px-4 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-gray-600 border border-transparent rounded-md shadow-sm hover:bg-gray-500 focus:outline-none focus:shadow-outline-blue active:bg-gray-600">
+          <button className="px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out bg-gray-600 border border-transparent rounded-md shadow-sm hover:bg-gray-500 focus:bg-gray-500 focus:outline-none focus:ring focus:ring-gray-200 focus:ring-opacity-50 active:bg-gray-600">
             Save
           </button>
         </div>
