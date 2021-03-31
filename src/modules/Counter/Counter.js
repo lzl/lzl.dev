11305030a2124f009e73e737c8711a548e42d434
lizunlong <lizunlong@gmail.com>
2021-04-01 00:56:10 +0800

    createSelector

--- src/modules/Counter/Counter.js
+++ src/modules/Counter/Counter.js
@@ -1,11 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { createSelector } from '@reduxjs/toolkit'
 
 import { counterActions } from '@/modules/Counter/slice'
 import Counter from '@/components/Counter/Counter'
 import Controller from '@/components/Counter/Controller'
 
+const countSelector = createSelector(
+  (state) => state.counter,
+  (counter) => counter.value
+)
+
 function CounterWrapper() {
-  const value = useSelector((state) => state.counter.value)
+  const value = useSelector(countSelector)
 
   return <Counter name="Counter" value={value} />
 }
