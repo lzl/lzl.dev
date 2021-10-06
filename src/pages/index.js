b84087a481e6fffa22e4a3fe6e753efffce26a87
lizunlong <lizunlong@gmail.com>
2021-10-07 00:45:00 +0800

    try upstash

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
+import Redis from 'ioredis'
 import LeftRight from '@/components/LeftRight'
 
-function Right() {
-  return <p>do the work</p>
+const redis = new Redis(process.env.REDIS_URL)
+
+function Right({ counter }) {
+  return <p>do the work ({counter})</p>
 }
 
-export default function Index() {
-  return <LeftRight right={<Right />} />
+export default function Index({ counter }) {
+  return <LeftRight right={<Right counter={counter} />} />
+}
+
+export async function getServerSideProps() {
+  const counter = await redis.incr('counter')
+  return { props: { counter } }
 }
