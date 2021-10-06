2764a379569b2baae347e5a7ed5a7f6584702a79
lizunlong <lizunlong@gmail.com>
2021-10-06 21:31:42 +0800

    Update state.js

--- src/pages/state.js
+++ src/pages/state.js
@@ -1,14 +1,14 @@
 import LeftRight from '@/components/LeftRight'
 import Counter from '@/modules/Counter/Counter'
-import GitHub from '@/modules/GitHub/GitHub'
-import StarWars from '@/modules/StarWars/StarWars'
+// import GitHub from '@/modules/GitHub/GitHub'
+// import StarWars from '@/modules/StarWars/StarWars'
 
 function Right() {
   return (
     <div className="space-y-4">
       <Counter />
-      <GitHub />
-      <StarWars />
+      {/* <GitHub /> */}
+      {/* <StarWars /> */}
     </div>
   )
 }
