3ac08a90c777ec771699736cb5960f796e222d62
lizunlong <lizunlong@gmail.com>
2021-10-06 23:48:03 +0800

    use Immer

--- src/modules/TodoList/store.ts
+++ src/modules/TodoList/store.ts
@@ -1,8 +1,10 @@
-import { useReducer } from 'react'
+import * as React from 'react'
+import produce, { current, Draft } from 'immer'
 import { createContainer } from 'react-tracked'
+import { nanoid } from '@reduxjs/toolkit'
 
 export type TodoType = {
-  id: number
+  id: string
   title: string
   completed?: boolean
 }
@@ -15,52 +17,47 @@ type State = {
 
 type Action =
   | { type: 'ADD_TODO'; title: string }
-  | { type: 'DELETE_TODO'; id: number }
-  | { type: 'TOGGLE_TODO'; id: number }
+  | { type: 'DELETE_TODO'; id: string }
+  | { type: 'TOGGLE_TODO'; id: string }
   | { type: 'SET_QUERY'; query: string }
 
 const initialState: State = {
   count: 0,
   todos: [
-    { id: 1, title: 'Wash dishes' },
-    { id: 2, title: 'Study JS' },
-    { id: 3, title: 'Buy ticket' },
+    { id: nanoid(), title: 'Wash dishes' },
+    { id: nanoid(), title: 'Study JS' },
+    { id: nanoid(), title: 'Buy ticket' },
   ],
   query: '',
 }
 
-let nextId = 4
-
-const reducer = (state: State, action: Action): State => {
+const reducer = (draft: Draft<State>, action: Action): void | State => {
   switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        ...state,
-        todos: [...state.todos, { id: nextId++, title: action.title }],
-      }
-    case 'DELETE_TODO':
-      return {
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      }
-    case 'TOGGLE_TODO':
-      return {
-        ...state,
-        todos: state.todos.map((todo) =>
-          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
-        ),
-      }
-    case 'SET_QUERY':
-      return {
-        ...state,
-        query: action.query,
-      }
+    case 'ADD_TODO': {
+      draft.todos.push({ id: nanoid(), title: action.title })
+      break
+    }
+    case 'DELETE_TODO': {
+      const idx = draft.todos.findIndex((todo) => todo.id === action.id)
+      draft.todos.splice(idx, 1)
+      break
+    }
+    case 'TOGGLE_TODO': {
+      const idx = draft.todos.findIndex((todo) => todo.id === action.id)
+      draft.todos[idx].completed = !draft.todos[idx].completed
+      break
+    }
+    case 'SET_QUERY': {
+      draft.query = action.query
+      break
+    }
     default:
-      return state
+      return current(draft)
   }
 }
 
-const useValue = () => useReducer(reducer, initialState)
+const producedReducer = produce(reducer)
+const useValue = () => React.useReducer(producedReducer, initialState)
 
 export const {
   Provider: TodoListProvider,
