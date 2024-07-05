9583f543501cd432050f04b1987748ad3789069d
lizunlong <lizunlong@gmail.com>
2024-07-05 22:40:12 +0800

    feat: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'lzl.dev',
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
