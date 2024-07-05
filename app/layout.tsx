e59eb0a39c4a0f53be4e4f6223ba38fdda71adbd
lizunlong <lizunlong@gmail.com>
2024-07-05 22:55:20 +0800

    chore: format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
   title: 'lzl.dev',
