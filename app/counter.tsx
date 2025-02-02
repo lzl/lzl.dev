d0a0891c87412b387528a48443c634b4cd368c54
lizunlong <lizunlong@gmail.com>
2025-02-02 16:50:47 +0800

    feat: use dynamic io

--- app/counter.tsx
+++ app/counter.tsx
@@ -1,19 +1,27 @@
-import { revalidateTag } from 'next/cache'
+import {
+  revalidateTag,
+  unstable_cacheLife as cacheLife,
+  unstable_cacheTag as cacheTag,
+} from 'next/cache'
 import CounterButton from './counter-button'
 
 const url = `${process.env.REDIS_API_URL}/incr/counter`
 const token = `Bearer ${process.env.REDIS_API_TOKEN}`
 
-export default async function Counter() {
+async function getCounter() {
+  'use cache'
+  cacheTag('counter')
+  cacheLife('seconds')
   const data = await fetch(url, {
     headers: {
       Authorization: token,
     },
-    cache: 'no-store',
-    next: {
-      tags: ['counter'],
-    },
   }).then((res) => res.json())
+  return data
+}
+
+export default async function Counter() {
+  const data = await getCounter()
 
   async function incrCounter() {
     'use server'
